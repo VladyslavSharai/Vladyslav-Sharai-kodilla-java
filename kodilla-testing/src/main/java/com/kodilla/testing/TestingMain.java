6ffce49ef344a47cf0f4dc64bfe5d094b68c4803
Vladysalv Sharai <vladsharai3@gmail.com>
2026-05-08 12:13:51 +0200

    6.2 Dodano class Calculator i stworzono do niego testy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        int a = 5;
+        int b = 5;
+        Calculator c = new Calculator();
+
+        System.out.println("Testing method add");
+        int resultAdd = c.add(a, b);
+        if(resultAdd == 10) {
+            System.out.println("Testing add: OK");
+        }
+        else  {
+            System.out.println("Testing add: FAIL");
+        }
+
+        System.out.println("Testing method subtract");
+        int resultSubtract = c.subtract(a, b);
+        if(resultSubtract == 0) {
+            System.out.println("Testing substract: OK");
+        }
+        else  {
+            System.out.println("Testing substract: FAIL");
+        }
     }
 }
