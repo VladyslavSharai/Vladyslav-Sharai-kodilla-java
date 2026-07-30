3172ab1faf6a04947f61d25d93f5da0f5a9f44a9
Vladysalv Sharai <vladsharai3@gmail.com>
2026-07-30 11:42:36 +0200

    7.2 Stworzylem class Forumuser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,15 +1,13 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-
-        poemBeautifier.beautify("Hello world", text -> text.toUpperCase());
-        poemBeautifier.beautify("Hello world", text -> "***" + text + "***");
-        poemBeautifier.beautify("Hello world", text -> text.replace(" ", "_"));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
